e007d028886974c05c3adbbd9a8361db3070ea5d
mrmadhat <danny@websitesbydanny.co.uk>
2019-02-01 16:27:05 +0000

    Added support for sub option pages

--- src/components/admin/App.js
+++ src/components/admin/App.js
@@ -1,16 +1,34 @@
 import React from 'react'
+/**
+ * HashRouter gets around the problem with the wordpress page url being a query string
+ * 
+ * see: https://wp-community-uk.slack.com/archives/C04DTD9AK/p1549036527252900
+ */
+import {HashRouter as Router, Route, Link} from "react-router-dom"
 
 import Header from './Header'
 import SocialNetworkSettings from '../../pages/admin/SocialNetworkSettings';
+import WelcomePage from '../../pages/admin/Welcome';
 
 const App = () => (	
-	<div>
-		<Header title="Relative Sharer - Options" tag="h1" className="relative-sharer-header--main" desc="Welcome to the Relative Sharer plugin options page" />
-		{/* // TODO: add a sidebar that holds links to the different settings pages available */}
+	<Router>
+		<div>
+			<Header title="Relative Sharer - Options" tag="h1" className="relative-sharer-header--main" desc="Welcome to the Relative Sharer plugin options page" />
+			<div className="relative-sharer-main-container">
 
-		{/* // TODO: add react router and make this a page  */}
-		<SocialNetworkSettings />
-	</div>
+				<div className="relative-sharer-sidebar">
+
+					<h3>Menu</h3>
+					<Link to="/">Welcome</Link>
+					<Link to="social-network-settings">Social Network Settings</Link>
+				</div>
+				{/* // TODO: add react router and make this a page  */}
+
+				<Route exact path="/" component={WelcomePage} />
+				<Route path="/social-network-settings" component={SocialNetworkSettings} />
+			</div>
+		</div>
+	</Router>
 )
 
 export default App
---
+++ src/pages/admin/Welcome/index.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+import Main from '../../../components/admin/Main';
+
+const WelcomePage = () => {
+  return (
+	<Fragment>
+		<Main title="Welcome to Relative sharer">
+			Some welcome stuff
+		</Main>
+	</Fragment>
+  )
+}
+
+export default WelcomePage
